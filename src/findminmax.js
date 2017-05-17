f98d9f30b94ab3e67e4941312ba0d692f2d65f12
Your Name <you@example.com>
2017-05-17 20:44:14 +0100

    add excisting file

---
+++ src/findminmax.js
@@ -0,0 +1,10 @@
+function findMin(array) {
+	return array.reduce(function(a,b) {
+		return math.min(a,b);
+		};
+	}
+function findMax(array) {
+	return array.reduce(function(a,b) {
+		return Math.max(a,b);
+	};
+}

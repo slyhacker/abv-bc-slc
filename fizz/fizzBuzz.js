aa43e11cbcf50e9b34b45e01173d7412cfd71a33
Your Name <you@example.com>
2017-05-17 22:24:12 +0100

    add existing file

---
+++ fizz/fizzBuzz.js
@@ -0,0 +1,14 @@
+var FizzBuzz = function(number) {
+	if(number % 3) === 0 {
+		return "Fizz"
+		else if(number % 5) === 0;{
+			return "Buzz"
+			else if(number % 15) === 0;{
+				return "FizzBuzz"
+				else
+					return number;
+			};
+
+
+
+	

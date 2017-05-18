e8513d50b9f907469f36331951bb60e63d74dd8c
Your Name <you@example.com>
2017-05-18 14:47:01 +0100

    Add existing file

--- README.md
+++
@@ -1,6 +0,0 @@
-# abv-bc-slc
-// Write a `getPrimes` function that takes an integer value `n` as argument and generates prime numbers from `0` to `n` with asymptotic analysis. 
-The generated prime numbers should be in an array. 
-If no prime number is obtainable for the input, return an empty array.
-Your logic should be as efficient as possible (with minimal iterations).
-Your code should also address edge cases (invalid input, negative input value, or zero as input)
---
+++ reverseString.js
@@ -0,0 +1,38 @@
+function reverseString(str) {
+	if (str === "")
+		return "";
+	
+function palindrome(str) {
+	retrun str ===st.split('').reverse().join('');
+}
+
+palindrome('anna')
+true
+
+palindrome('NaN')
+true
+
+palindrome('civic')
+
+true
+
+const reverse = (str) => {
+	for (let i= str.length; i<=str.length; i--){
+		return reverse}
+
+		console.assert(reverse('books') === 'skoob'
+		console.assert(reverse('solomon') === 'nomolos'
+		console.assert(reverse)('misc') === 'csim'	
+	}
+});
+
+	
+
+
+
+
+
+
+
+
+
